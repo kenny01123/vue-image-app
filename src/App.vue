9aaf9fcaa4ee197576c17d7cf2ef60a1b67e1205
Jose M. Torres <jmtorresr@gmail.com>
2020-10-26 14:58:26 +0900

    Initialized vue, setup the data structure and passed first props to children
    Co-authored-by: dhequex <dhequex@users.noreply.github.com>
    Co-authored-by: kenny01123 <kenny01123@users.noreply.github.com>

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,34 @@
 <template>
   <div id="app">
+    <div>
+   </div>
     <img alt="Vue logo" src="./assets/logo.png" />
     <h1>{{ title }}</h1>
-    <navbar />
+    <h2>{{currentView}}</h2>
+    <navbar :title="title"></navbar>
   </div>
 </template>
 
 <script>
 import Navbar from "./components/Navbar";
+import AllPhotos from "./components/AllPhotos";
+import SinglePhoto from "./components/SinglePhoto";
+
+
 
 export default {
   name: "App",
   components: {
-    navbar: Navbar
+    navbar: Navbar,
+    AllPhotos: AllPhotos,
+    SinglePhoto: SinglePhoto
   },
+  
   data: () => ({
-    title: "Photo Upload App"
+    title: "Photo Upload App",
+    currentView: "All Photos", 
+    selectedPhotos: "selected",
+    photos: [ 1, 2, 3],
   })
 };
 </script>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,11 +1,24 @@
 <template>
-  <div />
+  <div>
+    <h1>{{title}}</h1>
+    
+  </div>
 </template>
 
 <script>
+
+
+
 export default {
-  name: "Navbar"
+  name: "Navbar",
+  props: {
+    title: String,
+    currentView: String,
+    selectedPhotos: String,
+    photos: Array 
+    }
 };
 </script>
 
 <style></style>
+
